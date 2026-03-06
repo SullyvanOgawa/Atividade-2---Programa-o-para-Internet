1fa8cad84a7b0dfa404c93001a60e21ced06bf54
Sullyvan Ogawa <sullyvanogawa@gmail.com>
2026-03-06 04:03:21 -0300

    Alterando nome dos Profesores

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import Curso from './Model/curso.js';
 import Professor from './Model/professor.js';
 
-const professor1 = new Professor(1, 'Sullyvan', 'Especialidade em Python');
-const professor2 = new Professor(2, 'Almeida', 'Especialidade em Java');
-const professor3 = new Professor(3, 'José', 'Especialidade em Java Script para FrontEnd');
-const professor4 = new Professor(4, 'João', 'Especialidade em Persistência de dados com Node e mySQL');
+const professor1 = new Professor(1, 'Cássia Perego', 'Especialidade em Python');
+const professor2 = new Professor(2, 'Renato Gonçalves', 'Especialidade em Java');
+const professor3 = new Professor(3, 'Cristiane Rizzo', 'Especialidade em Java Script para FrontEnd');
+const professor4 = new Professor(4, 'Sullyvan Ogawa', 'Especialidade em Persistência de dados com Node e mySQL');
 
 const curso1 = new Curso(1, 'Python', 'Desenvolvimento backend', 40, professor1);
 const curso2 = new Curso(2, 'Java', 'Desenvolvimento backend', 40, professor2);
@@ -18,11 +18,11 @@ const curso4 = new Curso(4, "Node.js", 'Desenvolvimento backend', 40, professor4
 // })
 // .catch(err => console.log(err));
 
-// professor1.editar()
-// .then(() => {
-//     console.log("Curso editado com sucesso!!!");
-// })
-// .catch(err => console.log(err));
+professor4.editar()
+.then(() => {
+    console.log("Curso editado com sucesso!!!");
+})
+.catch(err => console.log(err));
 
 
 // curso.excluir()
